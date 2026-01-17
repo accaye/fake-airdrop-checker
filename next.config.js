ca2e1e11f117a6f2e4ce106f9c3acd0a69f59fcf
accaye <accachnei@gmail.com>
2026-01-17 22:07:59 +0700

    feat: add hosted manifest redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // opsional, untuk static export
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://farcaster.xyz/~/developers/hosted-manifests?domain=airdrop-checker-chi.vercel.app',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
